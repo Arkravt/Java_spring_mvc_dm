59bb859e4416a212e5bb74c28986ca625a95d688
Kravtsev Artem <arkravt@gmail.com>
2024-04-12 00:27:35 +0400

    12.04.2024

--- src/main/java/com/example/demo/controllers/members/MembersHandler.java
+++ src/main/java/com/example/demo/controllers/members/MembersHandler.java
@@ -1,17 +1,31 @@
 package com.example.demo.controllers.members;
 
+import com.example.demo.controllers.models.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/members")
 public class MembersHandler {
 
+    /*
+    Model - это то что передается в шаблон. Находясь в контроллере можно положить какие-то данные в Model и в шаблоне
+    эти данные использовать. Model по структуре очень похож на Map. У каждого метода своя область видимости и
+    поэтому у Model область видимости так же только в рамках метода.
+     */
+
+    private List<Member> members = new ArrayList<>();
+
     @GetMapping("")
-    public String members() {
+    public String members(Model model) {
+        model.addAttribute("members", members);
         return "members/members";
     }
 
@@ -23,7 +37,7 @@ public class MembersHandler {
     @PostMapping("/new_member/create")
     public String createNewMember(@RequestParam String name,
                                   @RequestParam String lastName) {
-        System.out.println(name + " " + lastName);
+        members.add(new Member(name, lastName));
         return "redirect:/members";
     }
 
---
+++ src/main/java/com/example/demo/controllers/models/Member.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers.models;
+
+public class Member {
+
+    private String firstName;
+    private String lastName;
+
+    public Member(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName='" + firstName  + ", lastName='" + lastName;
+    }
+}
